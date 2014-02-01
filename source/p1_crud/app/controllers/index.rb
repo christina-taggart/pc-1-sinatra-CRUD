8161941526f65a2e05ba3dc372d679f07f30c7c8
John Olmsted <johnaolmsted@gmail.com>
2014-01-31 19:08:13 -0800

    Wrote up RESTful skeleton for controller

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -2,7 +2,23 @@ get '/' do
   erb :index
 end
 
+#CREATE note
+post '/note' do
+
+end
+
+# READ note
 get '/note/:note_id' do
   @note = Note.find(params[:note_id])
   erb :note
+end
+
+# UPDATE note
+patch '/note/:note_id' do
+
+end
+
+# DELETE note
+delete '/note/:note_id' do
+
 end
