a4e509c42fe5bc27c06689d48ab1a1c7b53958db
Alexander Leishman <null>
2014-01-31 23:01:26 -0800

    Built out basic functionality for restful routes

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -1,16 +1,27 @@
 get '/' do
-  erb :index
+  redirect '/notes'
 end
 
 # Show links to all notes
 get '/notes' do
   @notes = Note.all
+  erb :notes
 end
 
 # Show single note
 get '/notes/:id' do
   @note = Note.find(params[:id])
   erb :note
+end
+
+# Show form for creating new note
+get 'notes/new' do
+  erb :new
+end
+
+# Post request for creating new note
+post '/notes' do
+  Note.create(params[:note])
 end
 
 # Show page for editing a note
@@ -21,10 +32,13 @@ end
 
 # Update an existing note
 put '/notes/:id' do
-
+  p params[:note]
+  Note.update(params[:id], params[:note])
+  redirect "/notes/#{params[:id]}"
 end
 
 # Delete an existing note
 delete '/notes/:id' do
-
+  Note.destroy(params[:id])
+  redirect "/notes"
 end
