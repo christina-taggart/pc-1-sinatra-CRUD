ddf169cdaade5554633f8b3bc4273ef7dec103c3
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-01 08:51:24 -0800

    Create view for note, user can create a new note and view titles of existing notes

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -3,6 +3,25 @@ get '/' do
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+get '/notes' do
+	# view all notes (by title)
+	erb :note
+end
+
+post '/notes' do
+	# creates a new note
+	Note.create(params)
+	erb :note
+end
+
+get '/notes/:id' do
+	# view specific note
+end
+
+put '/notes/:id' do
+	# update specific note
+end
+
+delete '/notes/:id' do
+	# delete specific note
 end
