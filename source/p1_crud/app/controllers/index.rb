bf3bdaa7b1a1c48d50f1addf8f1c3a0e960f5397
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-01 21:14:01 -0800

    Can now update notes

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -12,16 +12,19 @@ end
 post '/notes' do
 	# creates a new note
 	Note.create(params)
+	@partials = ["partials/create_note", "partials/existing_notes"]
 	erb :note
 end
 
 get '/notes/:id' do
-	@partials = ["partials/view_a_note"]
+	@partials = ["partials/view_a_note", "partials/update_a_note"]
+	p @note = Note.find(params[:id])
 	erb :note
 end
 
 put '/notes/:id' do
-	# update specific note
+	Note.find(params[:id]).update_attributes(id: params[:id], title: params[:title], content: params[:content])
+	redirect '/notes'
 end
 
 delete '/notes/:id' do
