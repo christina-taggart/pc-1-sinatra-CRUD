f127f101ed5b53ca8490c83939670e2193aa9628
David Goodman <goodman.david.h@gmail.com>
2014-02-02 15:35:30 -0800

    CRUD works

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -1,8 +1,56 @@
+#Home
 get '/' do
   # Look in app/views/index.erb
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+#View All Notes
+get '/notes' do
+  @notes = Note.all
+  erb :notes
 end
+
+#View Specific Note
+get '/notes/:id' do
+	p "#{:id}"
+	@note = Note.find(params[:id])
+	erb :note
+end
+
+#Create a new note
+ get '/note/new' do
+ 	erb :notes_new
+ end
+
+ post '/' do
+ 	@note = Note.create(params)
+ 	redirect '/'
+ end
+
+#Edit a note
+patch '/edit/:id' do
+	p params
+	@note = Note.find(params[:id])
+	@note[:title] = params[:title]
+	@note[:content] = params[:content]
+	@note.save
+	redirect '/notes'
+end
+
+get '/edit/:id' do
+	@note = Note.find(params[:id])
+	erb :edit
+end
+
+#Delete note
+delete '/delete/:id' do
+	@note = Note.find(params[:id])
+	redirect "/delete/#{:id}"
+end
+
+get '/delete/:id' do
+	@note = Note.find(params[:id])
+	@note.destroy
+	erb :delete
+end
+
---
+++ source/p1_crud/db/seeds.rb
@@ -0,0 +1,3 @@
+Note.create({title: "Dear Diary", content: "Toay was fabulous"})
+Note.create({title: "Cutest Boyzzzzz", content: "1-David, 2-That guy that looks like David, 1000-Aditya"})
+Note.create({title: "ThIs WoRkEd", content: "Because I am a super duper programmer"})
