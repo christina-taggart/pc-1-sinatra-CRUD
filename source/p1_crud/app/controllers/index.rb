8d5d032b5e964b1ed4683b6c5bc1dea35b109e5b
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-02-02 17:41:35 -0800

    cleaned out unnecessary skeleton files created migration for note table created model for notes created get route for index created post route for create note form display all notes on index page created edit button with route to edit page created a commit message that is too long

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -1,8 +1,27 @@
 get '/' do
   # Look in app/views/index.erb
+  @note = Note.all
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+#create new note
+post '/note' do
+	 Note.create(content: params[:note], title: params[:title])
+	 redirect "/"
+end	
+
+get '/note/:id' do
+	@note = Note.find(params[:id])	
+	erb :edit
+end
+#update note
+put '/:note_id' do
+ 
+redirect "/"
+end
+
+#delete note
+delete '/:note_id' do
+
+redirect '/'
 end
---
+++ source/p1_crud/db/migrate/20140202144259_create_notes.rb
@@ -0,0 +1,7 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+  	create_table :notes do |t|
+  		t.string :title, :content
+  	end
+  end
+end
