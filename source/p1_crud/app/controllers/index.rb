01c87e2361e764bc5357b9f5ee4373f443d4ba35
Apprentice <apprentice@dbc12.local>
2014-02-03 21:07:38 -0800

    Migrated and created database, added ability to view, add, and delete notes.

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -1,8 +1,20 @@
 get '/' do
-  # Look in app/views/index.erb
+  @notes = Note.all
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+post '/new_note' do
+  Note.create(title: params['note_title'], content: params['note_content'])
+  redirect '/'
+end
+
+delete '/note/:id' do
+  Note.destroy(params[:id])
+  redirect '/'
+end
+
+#actually, what I'd like to do is have a form pop up in page
+post '/note/:id/edit' do
+
+  redirect '/note/:id/edit'
 end
