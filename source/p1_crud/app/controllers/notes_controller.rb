1b3ce1710067d9b61fcd27b3f0f8726ed0fbfcaa
K Y <kevin@kevinyeh.com>
2014-02-04 20:09:20 -0800

    Add routes for notes

---
+++ source/p1_crud/app/controllers/notes_controller.rb
@@ -0,0 +1,20 @@
+get '/notes' do
+end
+
+get '/photos/new' do
+end
+
+post '/photos' do
+end
+
+get '/photos/:id' do
+end
+
+get '/photos/:id/edit' do
+end
+
+put '/photos/:id' do
+end
+
+delete '/photos/:id' do
+end
