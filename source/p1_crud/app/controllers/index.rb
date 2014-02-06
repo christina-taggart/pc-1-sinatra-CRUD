d36cd274e6c02b4426fbaedc8dfe3c5181ef50bb
Emily Foley <null>
2014-02-05 19:11:05 -0800

    Added partials

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -1,14 +1,15 @@
 get '/' do
+  @notes = Note.all
   erb :index
 end
 
 get '/notes' do
   @notes = Note.all
-  erb :allnotes
+  erb :_allnotes_partial
 end
 
 get '/notes/create' do
-  erb :create
+  erb :_create_note_partial
 end
 
 post '/notes/create' do
