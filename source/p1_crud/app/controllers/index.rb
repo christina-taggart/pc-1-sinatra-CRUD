1c0c8d2917acc33e329a266f1f27108fecefcba9
John Olmsted <johnaolmsted@gmail.com>
2014-02-06 15:08:17 -0800

    Renames routes to be more RESTful

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -8,34 +8,34 @@ get '/new_note' do
   erb :new_note
 end
 
-post '/new_note' do
+post '/notes' do
   new_note = Note.create(title: params[:title], content: params[:content])
-  redirect "/note/#{new_note.id}"
+  redirect "/notes/#{new_note.id}"
 end
 
 # READ note
-get '/note/:note_id' do
+get '/notes/:note_id' do
   @note = Note.find(params[:note_id])
   erb :note
 end
 
 # UPDATE note
-patch '/note/:note_id' do
+patch '/notes/:note_id' do
   updated_note = Note.where(id: params[:note_id]).first
   updated_note.title = params[:title]
   updated_note.content = params[:content]
   updated_note.save
-  redirect "/note/#{params[:note_id]}"
+  redirect "/notes/#{params[:note_id]}"
 end
 
 # DELETE note
-delete '/note/:note_id' do
+delete '/notes/:note_id' do
   if session[:delete_mode]
     Note.find(params[:note_id]).destroy
     session[:delete_mode] = nil
     redirect '/'
   else
     session[:delete_mode] = true
-    redirect "/note/#{params[:note_id]}"
+    redirect "/notes/#{params[:note_id]}"
   end
 end
