7e350c9517663b441e41617ac2887ecab7d49c2c
Christina Taggart <ctaggart@Christinas-MacBook-Pro-2.local>
2014-02-05 21:21:45 -0800

    Add Release 0: RESTful CRUD

--- source/p1_crud/app/controllers/index.rb
+++ source/p1_crud/app/controllers/index.rb
@@ -1,8 +1,46 @@
 get '/' do
-  # Look in app/views/index.erb
+	@notes = Note.all
   erb :index
 end
 
-get '/info' do
-  Demo.new(self).info
+get '/notes' do
+	@notes = Note.all
+
+	erb :index
+end
+
+get '/notes/new' do
+
+	erb :new
+end
+
+post '/notes' do
+	@title = params[:title]
+	@content = params[:content]
+	@new_note = Note.create(title: @title, content: @content)
+	erb :note_created
+end
+
+get '/notes/:id' do
+	@note = Note.find(params[:id])
+
+	erb :show
+end
+
+get '/notes/:id/edit' do
+	@note = Note.find(params[:id])
+
+	erb :edit
+end
+
+put '/notes/:id' do
+   note = Note.find(params[:id])
+   note.update_attributes({ title: params[:title], content: params[:content]})
+
+	redirect '/'
+end
+
+delete '/notes/:id' do
+	Note.find(params[:id]).destroy
+	redirect '/'
 end
