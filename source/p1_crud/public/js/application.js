203473f7d167242d015509bbd2450fa5494b5f9c
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-07 22:43:54 -0800

    Extracted inline js alert and placed it in script tags outside of the form

--- source/p1_crud/public/js/application.js
+++ source/p1_crud/public/js/application.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
 });
+
+
+<h2>Want to Delete this Note?</h2>
+<script> var confirmDelete = function() { alert('Are you sure you want to delete this note?'); }</script>
+  <form method='post' action='/notes/<%= @note.id %>' onsubmit="confirmDelete()">
+    <input type="hidden" name="_method" value="delete">
+    <button type='submit'>Delete!</button>
+  </form>
